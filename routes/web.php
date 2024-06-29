e320474511b3db8f2c174724df3628c4bbe94ac5
Pyae Phyo Khant <146649651+pyaephyokhant760@users.noreply.github.com>
2024-06-30 01:06:26 +0630

    login and register

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\user\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('auth',[AuthController::class,'auth'])->name('auth');
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified',])->group(function () {
+    //user
+    Route::get('user',[HomeController::class,'userPage'])->name('userPage');
 });
